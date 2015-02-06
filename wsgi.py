9315c05bce34ae97b2fa4fbf1cfaa9068cae4ec4
chriswood <chris314159@gmail.com>
2015-02-06 11:48:24 -0600

    wsgi bs

--- wsgi.py
+++ wsgi.py
@@ -2,9 +2,8 @@ import os
 import sys
 
 sys.path.append('/var/www/kiteeatingtree.org/')
-
+sys.path.append('/var/www/kiteeatingtree.org/ket2')
 os.environ['PYTHON_EGG_CACHE'] = '/var/www/kiteeatingtree.org/.python-egg'
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
-
-import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
